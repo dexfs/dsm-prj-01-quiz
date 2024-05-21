31c59ead07c5f77181235811e7a55563e750f340
Andre Santos <dexfs@pop-os.localdomain>
2024-05-20 21:47:03 -0300

    feat: add base styles config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+function MeuNome() {
+    return <h1>André Santos</h1>
+}
+function App() {
+
+  return (
+      <>
+          <h1>Hello world <MeuNome /></h1>
+      </>
+
+  )
+}
+
+export default App
